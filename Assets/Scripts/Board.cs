9f3ec4d4f413f24d8e7615ed082eea7d9d266924
Aionoff62 <Aionoff62@mail.ru>
2018-03-05 20:56:08 +0300

    Project has been created

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+    public int widthBoard;
+    public int hightBoard;
+    public GameObject[] tilePrefabs;
+
+    private BackgroundTile[,] tiles;
+    
+	void Start () {
+        tiles = new BackgroundTile[widthBoard, hightBoard];
+        SetUp();
+	}
+	
+    private void SetUp()
+    {
+        for (int i = 0; i < widthBoard; i++)
+        {
+            for (int j = 0; j < hightBoard; j++)
+            {
+                var position = new Vector2(i, j);
+                var tile = Instantiate(GetRandomTile(), position, Quaternion.identity);
+                tile.transform.parent = this.transform;
+                tile.name = "[" + i + ", " + j + "]";
+            }
+        }
+    }
+
+    private GameObject GetRandomTile()
+    {
+        int index = (int) Mathf.Round(Random.Range(0.0f, tilePrefabs.Length - 1));
+
+        return tilePrefabs[index];
+    }
+}
---
+++ Assets/Scripts/Dot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dot : MonoBehaviour {
+    public int row;
+    public int column;
+
+    private float swipeAngle = 0;
+    private Vector2 firstPosition;
+    private Vector2 secondPosition;
+    private GameObject otherDot;
+    private Board board;
+
+    // Use this for initialization
+    void Start () {
+        board = FindObjectOfType<Board>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        firstPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseUp()
+    {
+        secondPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CalculateAngle();
+    }
+
+    private void CalculateAngle()
+    {
+        swipeAngle = Mathf.Atan2(secondPosition.y - firstPosition.y, secondPosition.x - firstPosition.x) * 180/Mathf.PI;
+
+    }
+}
